bcdbc728b4b800388685d840807bc2001d78f139
Sebastian Lenz <github@sebastian-lenz.de>
2018-08-21 18:22:11 +0200

    Add wiki template

--- src/server/features/apiExplorer/DefinitionHandler.ts
+++ src/server/features/apiExplorer/DefinitionHandler.ts
@@ -1,4 +1,4 @@
-import Handler, { SymbolData, groupSymbols } from "./Handler";
+import Handler, { SymbolData, groupSymbols, sortSymbols } from "./Handler";
 import printParameterType from "../../projects/story/utils/printParameterType";
 import printParameterFlow from "../../projects/story/utils/printParameterFlow";
 import printSymbolType from "../../projects/story/utils/printSymbolType";
@@ -34,7 +34,7 @@ export default class DefinitionHandler extends Handler<Parameters> {
     return { category, location, signatures };
   }
 
-  async loadWiki(name: string, symbols: Array<SymbolData>) {
+  async loadWiki(name: string, symbols: Array<SymbolData>): Promise<any> {
     const parser = new WikiParser();
     const statusCode = await parser.load(name);
     const editLink = parser.getEditLink();
@@ -61,31 +61,37 @@ export default class DefinitionHandler extends Handler<Parameters> {
   }: Parameters): Promise<string> {
     await this.feature.loadPartial("breadcrumbs");
     await this.feature.loadPartial("definitions");
+    await this.feature.loadPartial("wiki-template");
 
     const signature = signatures[0];
     const type = printSymbolType(signature.type);
+    const plainSignatures = signatures.map(signature => ({
+      name: signature.name,
+      parameters: signature.parameters.map(parameter => ({
+        flow: parameter.flow ? printParameterFlow(parameter.flow) : undefined,
+        name: parameter.name,
+        type: parameter.type ? printParameterType(parameter.type) : undefined
+      }))
+    }));
 
     const allSymbols = await this.getSymbols(category.qualifiedName);
     const symbols = allSymbols.filter(symbol => symbol.name !== signature.name);
     const wiki = await this.loadWiki(signature.name, symbols);
+
+    if (wiki.notFound) {
+      wiki.category = type === "query" ? "Queries" : `${ucfirst(type)}s`;
+      wiki.name = signature.name;
+      wiki.plainType = type;
+      wiki.parameters = plainSignatures[0].parameters;
+      wiki.seeAlso = symbols.sort(sortSymbols);
+    }
 
     return this.render({
       location,
       template: "definition",
       context: {
         category,
-        signatures: signatures.map(signature => ({
-          name: signature.name,
-          parameters: signature.parameters.map(parameter => ({
-            flow: parameter.flow
-              ? printParameterFlow(parameter.flow)
-              : undefined,
-            name: parameter.name,
-            type: parameter.type
-              ? printParameterType(parameter.type)
-              : undefined
-          }))
-        })),
+        signatures: plainSignatures,
         symbols: groupSymbols(symbols),
         title: `${ucfirst(type)} ${signature.name}`,
         type,
